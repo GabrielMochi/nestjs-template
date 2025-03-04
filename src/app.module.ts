d97b855d989cc93110f19050dfb764e010268d1c
Gabriel Mochi <gmochi56@icloud.com>
2025-03-03 20:05:43 -0500

    remove pino logger due to incompatibility with express v5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfiguratorModule } from './infrastructure/modules/configurator.module';
-import { PinoLoggerModule } from './infrastructure/modules/pino-logger.module';
 import { HealthModule } from './interfaces/http/controllers/health/health.module';
 
 @Module({
-  imports: [ConfiguratorModule, PinoLoggerModule, HealthModule],
+  imports: [ConfiguratorModule, HealthModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-
   const configService = app.get(ConfigService);
-  const logger = app.get(Logger);
 
-  app.useLogger(logger);
+  app.setGlobalPrefix('api');
   app.use(helmet());
 
   const swaggerConfig = new DocumentBuilder()
@@ -24,7 +21,6 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, swaggerDocument, { jsonDocumentUrl: '/docs/json' });
 
   await app.listen(configService.get('process.port') ?? 3000);
-  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 void bootstrap();
