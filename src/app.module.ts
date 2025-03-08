72cc613626d4fae3fb84ddb4d9194c951b5942d0
Gabriel Mochi <gmochi56@icloud.com>
2025-03-07 20:46:47 -0500

    create path alias for common accessed folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './infrastructure/config/config.module';
-import { HealthModule } from './interfaces/http/controllers/health/health.module';
+import { ConfigModule } from '@infrastructure/config/config.module';
+import { HealthModule } from '@interfaces/http/controllers/health/health.module';
 
 @Module({
   imports: [ConfigModule, HealthModule],
