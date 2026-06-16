6aa7a1899a372ad135891abf6b48bb99e96ab0f3
Gabriel Mochi <gmochi56@icloud.com>
2026-06-16 00:32:48 -0300

    chore: update old `interface` layer alias to `presentation`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@infrastructure/config/config.module';
-import { HealthModule } from '@interfaces/http/controllers/health/health.module';
+import { HealthModule } from '@presentation/http/controllers/health/health.module';
 
 @Module({
   imports: [ConfigModule, HealthModule],
